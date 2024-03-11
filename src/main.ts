d5432c859828a2080c298c685fb95359a505267e
Italo Castillo <italogitdoe@gmail.com>
2024-03-11 21:07:17 +0100

    more functions

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
 import "./style.css";
 
+function appendToBody(elem: HTMLElement){
+  document.body.appendChild(elem);
+}
+
 function createParagraph(text: string = "<empty>"):HTMLParagraphElement {
   const p = document.createElement("p");
   p.textContent = text || "<imposible>";
@@ -14,7 +18,7 @@ and selfishness of heart.`));
 
 document.body.appendChild(createParagraph(`Choosing to be self-deceived,
 living under a false impression,
-they hope that Jehovah’s grace
+they hope that Jehovah\’s grace
 has been divinely imparted…`));
 
 document.body.appendChild(createParagraph());
@@ -29,3 +33,17 @@ function printMessage(text: string){
 
 const result = printMessage("Hey");
 console.log(result);
+
+
+function createElement(type: string, text: string){
+  const elem = document.createElement(type);
+  elem.textContent = text;
+  return elem;
+}
+
+const div = createElement('div', "Do you best");
+const span = createElement('span', " ✔✌")
+div.appendChild(span);
+appendToBody(div);
+
+console.log();
